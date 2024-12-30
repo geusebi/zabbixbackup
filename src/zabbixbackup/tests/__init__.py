32826f51dfaa1198821371593546f3c28647d6db
Giampaolo Eusebi <giampaolo.eusebi@gmail.com>
2024-12-30 20:29:27 +0100

    Disable expected warnings for tests

---
+++ src/zabbixbackup/tests/__init__.py
@@ -0,0 +1,2 @@
+# pylint: disable=missing-module-docstring,missing-function-docstring,missing-class-docstring
+# pylint: disable=unused-import
--- src/zabbixbackup/tests/test_backup.py
+++ src/zabbixbackup/tests/test_backup.py
@@ -1,1 +1,3 @@
+# pylint: disable=missing-module-docstring,missing-function-docstring,missing-class-docstring
+# pylint: disable=unused-import
 import unittest
