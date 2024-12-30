bd09444aacadeac0e70bbaa308950ef8f186fef9
Giampaolo Eusebi <giampaolo.eusebi@gmail.com>
2024-12-30 21:17:19 +0100

    Add test action

--- src/zabbixbackup/tests/test_archiver.py
+++ src/zabbixbackup/tests/test_archiver.py
@@ -1,3 +1,8 @@
 # pylint: disable=missing-module-docstring,missing-function-docstring,missing-class-docstring
 # pylint: disable=unused-import
 import unittest
+
+
+class TestTime(unittest.TestCase):
+    def test_dummy(self):
+        self.assertEqual(True, True)
