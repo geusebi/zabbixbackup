835c5dae876f25faa2f1a93d14778d60e835080e
Giampaolo Eusebi <giampaolo.eusebi@gmail.com>
2024-12-31 15:58:25 +0100

    Add build_tar_command basic tests

--- src/zabbixbackup/tests/test_utils.py
+++ src/zabbixbackup/tests/test_utils.py
@@ -3,13 +3,13 @@
 import unittest
 import logging
 from .. import console_logger
-from ..utils import build_compress_command
+from ..utils import build_compress_command, build_tar_command, check_binary
 
 
 console_logger.setLevel(logging.ERROR)
 
 
-class TestUtils(unittest.TestCase):
+class TestBuildCompress(unittest.TestCase):
     def test_compress_standard_cli(self):
         profile = ("xz", 6, ("some_params", ))
         result = build_compress_command(
@@ -65,6 +65,67 @@ class TestUtils(unittest.TestCase):
 
 
     def test_compress_not_available_cli(self):
+        profile = ("xz", 6, ("some_params", ))
+
+        with self.assertRaises(NotImplementedError):
+            build_compress_command(
+                profile, check=False, strategy=tuple())
+
+
+class TestBuildTar(unittest.TestCase):
+    def test_build_tar_cli(self):
+        with self.subTest("uncompressed tar"):
+            profile = ("tar", 6, ("some_params", ))
+            result = build_tar_command(profile, check=False)
+
+            expected = (
+                {},               # environment
+                ".tar",           # extension
+                ("tar", "-cf", ), # command
+            )
+
+            self.assertTupleEqual(result, expected)
+
+
+        with self.subTest("xz"):
+            profile = ("xz", 6, ("some_params", ))
+            result = build_tar_command(profile, check=False)
+
+            expected = (
+                {"XZ_OPT": "-6 some_params"}, # environment
+                ".tar.xz",                    # extension
+                ("tar", "-cJf", ),             # command
+            )
+
+            self.assertTupleEqual(result, expected)
+
+
+        with self.subTest("gzip"):
+            profile = ("gzip", 6, ("some_params", ))
+            result = build_tar_command(profile, check=False)
+
+            expected = (
+                {"GZIP": "-6 some_params"}, # environment
+                ".tar.gz",                  # extension
+                ("tar", "-czf", ),          # command
+            )
+
+            self.assertTupleEqual(result, expected)
+
+        with self.subTest("bzip2"):
+            profile = ("bzip2", 6, ("some_params", ))
+            result = build_tar_command(profile, check=False)
+
+            expected = (
+                {"BZIP2": "-6 some_params"}, # environment
+                ".tar.bz2",                  # extension
+                ("tar", "-cjf", ),           # command
+            )
+
+            self.assertTupleEqual(result, expected)
+
+
+    def test_tar_not_available_cli(self):
         profile = ("xz", 6, ("some_params", ))
 
         with self.assertRaises(NotImplementedError):
